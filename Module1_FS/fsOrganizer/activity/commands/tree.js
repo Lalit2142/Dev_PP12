d20f63d1226f7425deee1404024295a24a1407d0
Lalit <lalitaditya315@gmail.com>
2021-07-25 15:07:19 +0530

    final commit

--- Module1_FS/fsOrganizer/activity/commands/tree.js
+++ Module1_FS/fsOrganizer/activity/commands/tree.js
@@ -1,6 +1,26 @@
+let fs = require("fs") ;
+let path = require("path") ;
 
 function tree(src){
     console.log(`tree command executed with path ${src}`) ;
+    displayTree(src) ;
+}
+
+function displayTree(dirPath){
+
+    let dirContents = fs.readdirSync(dirPath) ;
+    for(let i = 0 ; i < dirContents.length ; i++){
+        let dirContent = dirContents[i] ;
+
+        let contentsPath = path.join(dirPath,dirContent) ;
+
+        if(fs.lstatSync(contentsPath).isDirectory()){
+            displayTree(contentsPath) ;
+        }
+        else{
+            console.log(dirContent) ;
+        }
+    }
 }
 
 module.exports = {
