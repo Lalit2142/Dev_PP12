a626ed1af70e3d0c4d67b35b198710187324d199
Lalit <lalitaditya315@gmail.com>
2021-08-14 13:45:16 +0530

    added callback hell code

--- Module2_WebScrapping/raw/async/3Files/asyncSerial.js
+++ Module2_WebScrapping/raw/async/3Files/asyncSerial.js
@@ -1,23 +1,36 @@
 const fs = require("fs") ;
 
 console.log("Before") ;
-fs.readFile("./f1.txt",cb) ;
+// fs.readFile("./f1.txt",cb) ;
 
-function cb(err,data){
+// function cb(err,data){
 
+//     console.log(data+"") ;
+
+//     fs.readFile("./f2.txt",cb1) ;
+// }
+
+// function cb1(err,data){
+//     console.log(data+"") ;
+
+//     fs.readFile("./f3.txt",cb2) ;
+// }
+
+// function cb2(err,data){
+//     console.log(data+"") ;
+// }
+
+// called as callback hell or pyramid of doom 
+
+fs.readFile("./f1.txt",function(err,data){
     console.log(data+"") ;
+    fs.readFile("./f2.txt",function(err,data){
+        console.log(data+"") ;
+        fs.readFile("./f3.txt",function(err,data){
+            console.log(data+"") ;
+        })
+    })
+}) 
 
-    fs.readFile("./f2.txt",cb1) ;
-}
-
-function cb1(err,data){
-    console.log(data+"") ;
-
-    fs.readFile("./f3.txt",cb2) ;
-}
-
-function cb2(err,data){
-    console.log(data+"") ;
-}
 
 console.log("After") ;
