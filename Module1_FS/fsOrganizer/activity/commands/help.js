5694da997cddeeabe706b4c93fc69dc2d04fefba
Lalit <lalitaditya315@gmail.com>
2021-08-31 10:29:27 +0530

    organize done

--- Module1_FS/fsOrganizer/activity/commands/help.js
+++ Module1_FS/fsOrganizer/activity/commands/help.js
@@ -2,8 +2,7 @@
 function help(){
     console.log("The available commands are") ;
     console.log("1.node main.js help") ;
-    console.log("2.node main.js tree dirPath") ;
-    console.log("3.node main.js organize dirPath") ;
+    console.log("2.node main.js organize dirPath") ;
 }
 
 module.exports={
